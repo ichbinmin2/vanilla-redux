f509d0b8111b744a5307a71d8142fdec1fcf8130
JiyeonMin <ichbinmin2@gmail.com>
2021-05-02 14:28:33 +0900

    Add : vanilla-redux toDo learning

--- src/index.js
+++ src/index.js
@@ -1,42 +1,18 @@
-import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-// reducer는 data를 수정(modify)하는 함수이다
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
+const createTodo = (toDo) => {
+  const li = document.createElement("li");
+  li.innerText(toDo);
+  ul.appendChild(li);
 };
 
-// store(data를 넣어두는 장소)를 생성하고
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = (event) => {
+  event.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createTodo(toDo);
 };
 
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
-
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("click", onSubmit);
